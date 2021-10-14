c4a2405f5853cdc2b1b66cc2937763e94d486822
xliii <xliii@mail.com>
2021-10-14 15:40:40 +0300

    Enemies partially replaced + cleanup

--- main.js
+++ main.js
@@ -5,15 +5,15 @@ canvas.height = 800;
 ctx.textAlign = "center";
 
 let levels = [
-    new Level(1, "Vanilla", ['cloud1', 'cloud2', 'cloud3'], 2),
-    new Level(2, "Burning Pissade", ['cloud1', 'cloud2', 'cloud3'], 1),
-    new Level(3, "Wrath of the Piss King", ['cloud1', 'cloud2', 'cloud3'], 1),
-    new Level(4, "PeePeeClysm", ['cloud1', 'cloud2', 'cloud3'], 1),
-    new Level(5, "Mists of Pissdaria", ['cloud1', 'cloud2', 'cloud3'], 1),
-    new Level(6, "Warlords of Wankinor", ['cloud1', 'cloud2', 'cloud3'], 1),
-    new Level(7, "Jerkion", ['cloud1', 'cloud2', 'cloud3'], 1),
-    new Level(8, "Battle for Onanism", ['cloud1', 'cloud2', 'cloud3'], 1),
-    new Level(9, "Shadowanks", ['cloud1', 'cloud2', 'cloud3'], 1),  
+    new Level(1, "Vanilla", ['cloud1', 'cloud2', 'cloud3'], 1),
+    new Level(2, "Burning Pissade", ['cloud1'], 1),
+    new Level(3, "Wrath of the Piss King", ['cloud1'], 1),
+    new Level(4, "PeePeeClysm", ['cloud1'], 1),
+    new Level(5, "Mists of Pissdaria", ['cloud1'], 1),
+    new Level(6, "Warlords of Wankinor", ['cloud1'], 1),
+    new Level(7, "Jerkion", ['cloud1'], 1),
+    new Level(8, "Battle for Onanism", ['cloud1'], 1),
+    new Level(9, "Shadowanks", ['cloud1'], 1),
     new Level("credits", "Congratulations!", [], 0, 0)
 ];
 
